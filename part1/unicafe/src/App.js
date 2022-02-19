5554945aace159e05bfc364549c4ce4421f8d294
Eduardo <eduardo@sanp.com.br>
2022-02-19 13:55:13 -0300

    step 1

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div className="App">
+      <Header text='Give Feedback'/>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <div>
+        <Header text='Statistics' />
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
