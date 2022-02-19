c9581d7d0bbff6111876d36b9c5359750265f816
Eduardo <eduardo@sanp.com.br>
2022-02-19 14:11:34 -0300

    step 2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 
@@ -6,6 +6,22 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  useEffect(() => {
+    setAll(good + bad + neutral)
+    console.log(all)
+  }, [good, neutral, bad, all])
+
+  useEffect(() => {
+    setAvg((good - bad) / all)
+  }, [good, bad, all])
+
+  useEffect(() => {
+    setPositive((good / all) * 100)
+  }, [good, all])
 
   return (
     <div className="App">
@@ -18,6 +34,9 @@ function App() {
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
+        <p>All: {all}</p>
+        <p>Average: {avg}</p>
+        <p>Positive: {positive} %</p>
       </div>
     </div>
   );
