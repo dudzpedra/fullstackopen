ef5d6de4471066f10e1dcc3fbcff3a1217509969
Eduardo <eduardo@sanp.com.br>
2022-02-19 14:22:38 -0300

    step 4

--- part1/unicafe/src/components/Statistics/index.jsx
+++ part1/unicafe/src/components/Statistics/index.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Header from "../Header";
 const Statistics = (props) => {
+  if (props.good !== 0 || props.neutral !== 0 || props.bad !== 0) {
+    return (
+      <div>
+        <Header text="Statistics" />
+        <p>Good: {props.good}</p>
+        <p>Neutral: {props.neutral}</p>
+        <p>Bad: {props.bad}</p>
+        <p>All: {props.all}</p>
+        <p>Average: {props.avg}</p>
+        <p>Positive: {props.positive} %</p>
+      </div>
+    );
+  }
   return (
     <div>
       <Header text="Statistics" />
-      <p>Good: {props.good}</p>
-      <p>Neutral: {props.neutral}</p>
-      <p>Bad: {props.bad}</p>
-      <p>All: {props.all}</p>
-      <p>Average: {props.avg}</p>
-      <p>Positive: {props.positive} %</p>
+      <p>No feedback given</p>
     </div>
   );
 };
