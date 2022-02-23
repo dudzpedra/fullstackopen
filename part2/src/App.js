035926a26e0066ddea4c0b601dbecce447d97c5c
Eduardo <eduardo@sanp.com.br>
2022-02-23 13:21:42 -0300

    2.4 step 9 and 2.5

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,41 +2,63 @@ import './App.css';
 import Course from './components/Course';
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 13,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   const sumReducer = (total, part) => {
     return total + part.exercises
   }
   
-  const sum = course.parts.reduce(sumReducer, 0)
+  const sum = courses.map(course => course.parts.reduce(sumReducer, 0))
   console.log('sum: ', sum)
 
-  return <Course course={course} sum={sum} />
+  return (
+    <div>
+      {courses.map((course, i) => <Course key={course.id} course={course} sum={sum[i]} />)}
+    </div>
+  )
 }
 
 export default App;
