6efce2f63f38157e4658b280e190e20f16ffc139
Eduardo <eduardo@sanp.com.br>
2022-03-15 23:22:07 -0300

    4.5 step 3

--- part4/tests/likes.test.js
+++ part4/tests/likes.test.js
@@ -1,59 +1,90 @@
-const { expect } = require("@jest/globals");
+const { expect, describe } = require("@jest/globals");
 const listHelper = require("../utils/list_helper");
 
+const emptyList = [];
+
+const listOfOne = [
+  {
+    _id: "62311f1c909d7ccf66006080",
+    title: "My first blog",
+    author: "Dudz Pedra",
+    url: "https://github.com/dudzpedra",
+    likes: 26,
+    __v: 0,
+  },
+];
+
+const listOfMany = [
+  {
+    _id: "62311f1c909d7ccf66006080",
+    title: "My first blog",
+    author: "Dudz Pedra",
+    url: "https://github.com/dudzpedra",
+    likes: 26,
+    __v: 0,
+  },
+  {
+    _id: "62311f1c909d7ccf66006080",
+    title: "My first blog",
+    author: "Dudz Pedra",
+    url: "https://github.com/dudzpedra",
+    likes: 24,
+    __v: 0,
+  },
+  {
+    _id: "62311f1c909d7ccf66006080",
+    title: "My first blog",
+    author: "Dudz Pedra",
+    url: "https://github.com/dudzpedra",
+    likes: 50,
+    __v: 0,
+  },
+];
+
 describe("total likes", () => {
-  const emptyList = [];
+  test("when list has one blog, equals the likes of that ", () => {
+    const result = listHelper.totalLikes(listOfOne);
+    expect(result).toBe(26);
+  });
 
-  const listOfOne = [
-    {
+  test("of empty list equals 0", () => {
+    const result = listHelper.totalLikes(emptyList);
+    expect(result).toBe(0);
+  });
+
+  test("of list of many is calculated right", () => {
+    const result = listHelper.totalLikes(listOfMany);
+    expect(result).toBe(100);
+  });
+});
+
+describe("favourite blog", () => {
+  test("of empty list equals 0", () => {
+    const result = listHelper.favouriteBlog(emptyList);
+    expect(result).toEqual(0);
+  });
+
+  test("when list has one blog, equals that blog", () => {
+    const result = listHelper.favouriteBlog(listOfOne);
+    expect(result).toEqual({
       _id: "62311f1c909d7ccf66006080",
       title: "My first blog",
       author: "Dudz Pedra",
       url: "https://github.com/dudzpedra",
       likes: 26,
       __v: 0,
-    },
-  ];
+    });
+  });
 
-  const listOfMany = [
-    {
-      _id: "62311f1c909d7ccf66006080",
-      title: "My first blog",
-      author: "Dudz Pedra",
-      url: "https://github.com/dudzpedra",
-      likes: 26,
-      __v: 0,
-    },
-    {
-      _id: "62311f1c909d7ccf66006080",
-      title: "My first blog",
-      author: "Dudz Pedra",
-      url: "https://github.com/dudzpedra",
-      likes: 24,
-      __v: 0,
-    },
-    {
+  test("of list of many is calculated right", () => {
+    const result = listHelper.favouriteBlog(listOfMany);
+    expect(result).toEqual({
       _id: "62311f1c909d7ccf66006080",
       title: "My first blog",
       author: "Dudz Pedra",
       url: "https://github.com/dudzpedra",
       likes: 50,
       __v: 0,
-    },
-  ];
-
-  test("when list has one blog, equals the likes of that ", () => {
-    const result = listHelper.totalLikes(listOfOne);
-    expect(result).toBe(26);
+    });
   });
-
-  test('of empty list equals 0', () => {
-      const result = listHelper.totalLikes(emptyList)
-      expect(result).toBe(0)
-  })
-
-  test('of list of many is calculated right', () => {
-      const result = listHelper.totalLikes(listOfMany)
-      expect(result).toBe(100)
-  })
 });
